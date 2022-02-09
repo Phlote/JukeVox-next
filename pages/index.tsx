96b7a4feb92bbc417a4e742f3b98adb8e9f6eaa5
Michael Anderjaska <m.anderjaska@gmail.com>
2022-02-09 19:35:32 +0000

    pls build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,6 @@
-import { useWeb3React } from "@web3-react/core";
-import Head from "next/head";
-import Link from "next/link";
-import React, { useState } from "react";
-import Account from "../components/Account";
-import {
-  CurationMetadata,
-  CuratorSubmissionModal,
-} from "../components/CuratorSubmissionModal";
-import ETHBalance from "../components/ETHBalance";
-import { NavBar } from "../components/NavBar";
-import TokenBalance from "../components/TokenBalance";
-import useEagerConnect from "../hooks/useEagerConnect";
-import { useIsCurator } from "../hooks/useIsCurator";
-import { changeNetwork } from "../util";
+import React from "react";
 import Image from "next/image";
-import { LineInput, LineInputContainer } from "../components/LineInput";
-import { POLYGON_CHAIN_ID } from "../constants";
 import { HollowInput, HollowInputContainer } from "../components/Hollow";
-import { atom, useAtom } from "jotai";
 import { HomeLayout } from "../components/Layouts";
 
 function Home() {
