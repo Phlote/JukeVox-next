d2440fdf81fea8144fde308dafd9f2438a9cf294
Michael Anderjaska <m.anderjaska@gmail.com>
2022-02-11 15:04:27 +0000

    deploy script

---
+++ solidity/scripts/deploy.js
@@ -0,0 +1,23 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const PhloteContractFactory = await ethers.getContractFactory("Phlote");
+  const contract = await PhloteContractFactory.deploy();
+
+  await contract.deployed();
+
+  console.log("Token address:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
