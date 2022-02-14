a99136821727fc010bd8f6972a2672bbc7024bc7
Michael Anderjaska <m.anderjaska@gmail.com>
2022-02-14 18:30:19 +0000

    rip

--- contracts/addresses.tsx
+++ contracts/addresses.tsx
@@ -1,4 +1,4 @@
 // When we are ready to productionize, these should be env vars. Host these env vars on vercel and have them pulled
 
 export const NFT_MINT_CONTRACT_RINKEBY =
-  "0x1A60bbb38d2c658Ab689075aEF17fC48E2321ecb";
+  "0x3B31B9D8eBF22534CBeC3ba58dCBE6F93476D79B";
