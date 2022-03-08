0a56570d22d3192cea028a3136b361eed6e85924
Michael Anderjaska <m.anderjaska@gmail.com>
2022-03-08 16:18:14 +0000

    partway done transforming hollow stuff

--- components/Hollow/index.tsx
+++ components/Hollow/index.tsx
@@ -1,44 +1,29 @@
 import tw from "twin.macro";
 import styled from "styled-components";
 
-export const HollowButtonContainer = styled.div`
-  background: ${(props) => props.backgroundColor ?? "rgba(0, 0, 0, 0.47)"};
-
-  color: white;
-  padding: 1rem;
-  border-radius: 9999px;
-
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  max-height: 3.75rem;
-  border: 1px white solid;
-
-  &:focus {
-    outline: none;
-  }
-`;
+export const HollowButtonContainer = tw.div`bg-phlote-container text-white p-4 rounded-full flex flex-row items-center justify-center border border-white max-h-14`;
 
 export const HollowInputContainer = styled.div(({ type }) => [
   tw`bg-phlote-container text-white p-4 rounded-full flex flex-grow flex-row items-center justify-start`,
   type === "form" && tw`border border-white max-h-14`,
 ]);
 
+// export const HollowInput = styled.input`
+//   background-color: transparent;
+//   opacity: 100%;
+//   color: white;
+//   flex-grow: 1;
+//   max-width: 100%;
+//   outline: none;
+//   border: none;
+//   padding-left: 1rem;
+
+//   &:focus {
+//     outline: none;
+//   }
+// `;
+
 export const HollowInput = styled.input`
-  background-color: transparent;
-  opacity: 100%;
-  color: white;
-  flex-grow: 1;
-  max-width: 100%;
-  outline: none;
-  border: none;
-  padding-left: 1rem;
-
-  &:focus {
-    outline: none;
-  }
-
   &:-webkit-autofill,
   &:-webkit-autofill:focus {
     transition: background-color 600000s 0s, color 600000s 0s;
@@ -47,6 +32,7 @@ export const HollowInput = styled.input`
   &::placeholder {
     color: white;
   }
+  ${tw`bg-transparent opacity-100 text-white flex-grow max-w-full outline-none pl-4`}
 `;
 
 export const HollowButton = styled.button`
