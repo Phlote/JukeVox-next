5aa908330f52892c46f47502be78afb6043b7a3e
Michael Anderjaska <m.anderjaska@gmail.com>
2022-03-22 16:22:06 +0000

    this never had to be based on queries

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,6 @@
 import { useWeb3React } from "@web3-react/core";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ProfileSettingsForm } from "../components/Forms/ProfileSettingsForm";
@@ -10,20 +9,15 @@ import { ProfileLayout } from "../components/Layouts";
 
 function Profile() {
   const router = useRouter();
-  const { wallet } = router.query;
   const { account } = useWeb3React();
-
-  React.useEffect(() => {
-    if (account !== wallet) router.replace("/");
-  }, [account, router, wallet]);
 
   return (
     <ProfileLayout>
-      <ProfileSettingsForm wallet={wallet} />
+      <ProfileSettingsForm wallet={account} />q
       <ToastContainer position="bottom-right" autoClose={5000} />
       <HollowButtonContainer
         className="absolute bottom-10 right-10 cursor-pointer"
-        onClick={() => router.push(`/myarchive?wallet=${wallet}`)}
+        onClick={() => router.push(`/myarchive?wallet=${account}`)}
       >
         <HollowButton>
           View My Curations{" "}
