ab939e663f39697a0c8e750905e622fae014e1ca
Michael Anderjaska <m.anderjaska@gmail.com>
2022-03-30 17:34:01 +0100

    rm middleware

--- pages/_middleware.ts
+++
@@ -1,5 +0,0 @@
-import type { NextFetchEvent, NextRequest } from "next/server";
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  // check if the user is a curator (we need to know their wallet)
-}
