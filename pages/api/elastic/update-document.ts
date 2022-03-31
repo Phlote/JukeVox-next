6ddaa9e3581f26aa56bb0b8444679f6cdd957cb7
Michael Anderjaska <m.anderjaska@gmail.com>
2022-03-31 14:47:30 +0100

    drop in from previous branch

---
+++ pages/api/elastic/update-document.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { nodeElasticClient } from "../../../utils/elastic-app-search";
+
+const { ELASTIC_ENGINE_NAME } = process.env;
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const { editionId, documentId } = request.body;
+  try {
+    const res = await nodeElasticClient.updateDocuments(ELASTIC_ENGINE_NAME, [
+      {
+        id: documentId,
+        edition_id: editionId,
+      },
+    ]);
+
+    console.log(res);
+    response.status(200).send({ res });
+  } catch (e) {
+    console.error(e);
+    response.status(500).send(e);
+  }
+}
--- types/curations.ts
+++ types/curations.ts
@@ -32,3 +32,16 @@ export interface CurationNFTMetadata {
     };
   };
 }
+
+export interface CurationElasticSearchDocument {
+  supabase_id: number;
+  media_type: MediaType;
+  artist_name: string;
+  artist_wallet: string;
+  curator_wallet: string;
+  media_title: string;
+  media_uri: string;
+  marketplace: string;
+  tags?: string[];
+  submission_time: number;
+}
