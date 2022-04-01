78c152859f2481aa8b3947bdea081888099395ed
Michael Anderjaska <m.anderjaska@gmail.com>
2022-04-01 16:54:17 +0100

    fixed?

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -12,25 +12,35 @@ export default async function handler(
   const { searchTerm, filters } = request.body;
 
   try {
-    const res = await nodeElasticClient.search(
-      ELASTIC_ENGINE_NAME,
-      searchTerm as string,
-      { filters: curationToElasticSearchDocument(filters) }
-    );
+    let query = supabase.from("submissions").select();
 
-    if (res.results.length === 0) response.status(200).send({ results: [] });
+    console.log("searchTerm: ", searchTerm);
+    console.log("filters: ", filters);
 
-    const ids = res.results.map((document) =>
-      parseInt(document.supabase_id.raw)
-    ) as number[];
+    if (filters) {
+      query = query.match(filters);
+    }
 
-    const { data, error } = await supabase
-      .from("submissions")
-      .select()
-      .in("id", ids);
+    if (searchTerm && searchTerm.length > 0) {
+      const res = await nodeElasticClient.search(
+        ELASTIC_ENGINE_NAME,
+        searchTerm as string,
+        { filters: curationToElasticSearchDocument(filters) }
+      );
 
-    const searchResults = data.reverse();
+      if (res.results.length === 0) response.status(200).send({ results: [] });
 
+      const ids = res.results.map((document) =>
+        parseInt(document.supabase_id.raw)
+      ) as number[];
+
+      query = query.in("id", ids);
+    }
+
+    const { data, error } = await query;
+    const searchResults = data.sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
     console.log("search results: ", searchResults);
 
     if (error) throw error;
