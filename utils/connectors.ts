e315a0745897473f53c6cb26040c92852b9351c8
Michael Anderjaska <m.anderjaska@gmail.com>
2022-04-01 18:31:34 +0100

    working with rainbow, would need some hand holding for setup

--- utils/connectors.ts
+++ utils/connectors.ts
@@ -1,14 +1,13 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-
-const SUPPORTED_CHAIN_IDS = [4];
+import { SupportedChainId } from "./constants";
 
 export const Injected = new InjectedConnector({
-  supportedChainIds: SUPPORTED_CHAIN_IDS,
+  supportedChainIds: [SupportedChainId.RINKEBY],
 });
 
 export const WalletConnect = new WalletConnectConnector({
-  supportedChainIds: SUPPORTED_CHAIN_IDS,
+  supportedChainIds: [SupportedChainId.RINKEBY],
   rpc: {
     // 137: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_POLYGON}`,
     4: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,26 +1,6 @@
 export const TEST_PHOTE_TOKEN_ADDRESS =
   "0x31DA0475d29a452DA24Eb2ed0d41AD53E576b780";
 
-export const RINKEBY_CHAIN_ID = 4;
-export const POLYGON_CHAIN_ID = 137;
-
-export const NETWORKS = {
-  polygon: {
-    chainId: `0x${Number(POLYGON_CHAIN_ID).toString(16)}`,
-    chainName: "Polygon Mainnet",
-    nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
-      decimals: 18,
-    },
-    rpcUrls: ["https://polygon-rpc.com/"],
-    blockExplorerUrls: ["https://polygonscan.com/"],
-  },
-};
-
-export const PHLOTE_SIGNATURE_REQUEST_MESSAGE =
-  "Sign this message to prove you have access to this wallet. This won't cost you any gas. \n\n";
-
 export enum SupportedChainId {
   // MAINNET = 1,
   // ROPSTEN = 3,
@@ -51,3 +31,20 @@ export const CHAIN_IDS_TO_NAMES = {
   // [SupportedChainId.OPTIMISM]: "optimism",
   // [SupportedChainId.OPTIMISTIC_KOVAN]: "optimistic_kovan",
 };
+
+export const NETWORKS = {
+  polygon: {
+    chainId: `0x${SupportedChainId.POLYGON.toString(16)}`,
+    chainName: "Polygon Mainnet",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://polygon-rpc.com/"],
+    blockExplorerUrls: ["https://polygonscan.com/"],
+  },
+};
+
+export const PHLOTE_SIGNATURE_REQUEST_MESSAGE =
+  "Sign this message to prove you have access to this wallet. This won't cost you any gas. \n\n";
