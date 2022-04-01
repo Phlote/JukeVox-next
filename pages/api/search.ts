68870f5f40a4226d9097675a694f19bdf93612a2
Michael Anderjaska <m.anderjaska@gmail.com>
2022-04-01 17:14:08 +0100

    search bugs (#56)
    * fixed?
    
    * invert
    
    * lol at typings

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { nodeElasticClient } from "../../lib/elastic-app-search";
 import { supabase } from "../../lib/supabase";
+import { Curation } from "../../types/curations";
 import { curationToElasticSearchDocument } from "../../utils";
 
 const { ELASTIC_ENGINE_NAME } = process.env;
@@ -12,25 +13,38 @@ export default async function handler(
   const { searchTerm, filters } = request.body;
 
   try {
-    const res = await nodeElasticClient.search(
-      ELASTIC_ENGINE_NAME,
-      searchTerm as string,
-      { filters: curationToElasticSearchDocument(filters) }
-    );
+    let query = supabase.from("submissions").select();
 
-    if (res.results.length === 0) response.status(200).send({ results: [] });
+    console.log("searchTerm: ", searchTerm);
+    console.log("filters: ", filters);
 
-    const ids = res.results.map((document) =>
-      parseInt(document.supabase_id.raw)
-    ) as number[];
+    if (filters) {
+      query = query.match(filters);
+    }
 
-    const { data, error } = await supabase
-      .from("submissions")
-      .select()
-      .in("id", ids);
+    if (searchTerm && searchTerm.length > 0) {
+      const res = await nodeElasticClient.search(
+        ELASTIC_ENGINE_NAME,
+        searchTerm as string,
+        { filters: curationToElasticSearchDocument(filters) }
+      );
 
-    const searchResults = data.reverse();
+      if (res.results.length === 0) response.status(200).send({ results: [] });
 
+      const ids = res.results.map((document) =>
+        parseInt(document.supabase_id.raw)
+      ) as number[];
+
+      query = query.in("id", ids);
+    }
+
+    const { data, error } = await query;
+    const searchResults = data.sort((a: Curation, b: Curation) => {
+      return (
+        new Date(b.submissionTime).getTime() -
+        new Date(a.submissionTime).getTime()
+      );
+    });
     console.log("search results: ", searchResults);
 
     if (error) throw error;
