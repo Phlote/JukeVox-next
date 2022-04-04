ec931a7c265081f2ed83e2fc6d6f0e29faf67c17
Michael Anderjaska <m.anderjaska@gmail.com>
2022-04-04 13:00:52 +0100

    this is ok, but we can just set a cookie, no?

--- hooks/web3/useEagerConnect.ts
+++ hooks/web3/useEagerConnect.ts
@@ -1,6 +1,6 @@
 import { useWeb3React } from "@web3-react/core";
 import { useEffect, useState } from "react";
-import { Injected } from "../../utils/connectors";
+import { Injected, WalletConnect } from "../../utils/connectors";
 
 export default function useEagerConnect() {
   const { activate, active } = useWeb3React();
@@ -8,16 +8,20 @@ export default function useEagerConnect() {
   const [tried, setTried] = useState(false);
 
   useEffect(() => {
-    Injected.isAuthorized().then((isAuthorized) => {
-      if (isAuthorized) {
-        activate(Injected, undefined, true).catch(() => {
-          setTried(true);
-        });
-      } else {
-        setTried(true);
-      }
-    });
-  }, [activate]);
+    if (!tried && !active)
+      Injected.isAuthorized().then((isAuthorized) => {
+        if (isAuthorized) {
+          activate(Injected, undefined, true).catch(() => {
+            setTried(true);
+          });
+        } else {
+          // if injected isn't authorized, try wallet connect
+          activate(WalletConnect, undefined, true).catch(() => {
+            setTried(true);
+          });
+        }
+      });
+  }, [activate, tried, active]);
 
   // if the connection worked, wait until we get confirmation of that to flip the flag
   useEffect(() => {
