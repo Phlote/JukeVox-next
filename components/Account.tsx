444b2af1705a74b3997d47af75c1c213f372c77e
Michael Anderjaska <m.anderjaska@gmail.com>
2022-04-12 17:56:28 +0100

    Edit Profile goes under dropdown (#63)
    * done
    
    * tweak

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,7 +1,9 @@
 import { useWeb3React } from "@web3-react/core";
 import { UserRejectedRequestError } from "@web3-react/injected-connector";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
+import { useOnClickOut } from "../hooks/useOnClickOut";
 import { useOnCopy } from "../hooks/useOnCopy";
 import useENSName from "../hooks/web3/useENSName";
 import useMetaMaskOnboarding from "../hooks/web3/useMetaMaskOnboarding";
@@ -36,6 +38,11 @@ const Account = ({ triedToEagerConnect }: AccountProps) => {
 
   const ENSName = useENSName(account);
   const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+
+  const ref = useRef();
+  useOnClickOut(ref, () => setDropdownOpen(false));
+
+  const router = useRouter();
 
   if (error) {
     console.log(error);
@@ -90,6 +97,7 @@ const Account = ({ triedToEagerConnect }: AccountProps) => {
 
   return (
     <HollowInputContainer
+      ref={ref}
       className="h-full"
       style={{ justifyContent: "center" }}
     >
@@ -104,7 +112,7 @@ const Account = ({ triedToEagerConnect }: AccountProps) => {
         width={16}
       />
       {dropdownOpen && (
-        <DropdownActions bottom={-80}>
+        <DropdownActions bottom={-140}>
           <div
             className="cursor-pointer m-4 text-center text-xl hover:opacity-50 flex items-center"
             onClick={() => {
@@ -116,6 +124,18 @@ const Account = ({ triedToEagerConnect }: AccountProps) => {
             <div className="w-4" />
             <Image src="/exit.svg" alt={"disconnect"} height={24} width={24} />
           </div>
+          <div className="w-4" />
+          <div
+            onClick={() => {
+              router.push("/editprofile");
+              setDropdownOpen(false);
+            }}
+          >
+            <div className="cursor-pointer m-4 text-center text-xl hover:opacity-50 flex items-center">
+              Edit Profile
+            </div>
+          </div>
+          <div className="w-4" />
         </DropdownActions>
       )}
     </HollowInputContainer>
