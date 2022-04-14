8bd8bf330ef7fba4611cb42f655fad249a30d018
Michael Anderjaska <m.anderjaska@gmail.com>
2022-04-14 18:07:32 +0100

    hamburger

--- components/Account.tsx
+++ components/Account.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import { useRef, useState } from "react";
 import { useOnClickOut } from "../hooks/useOnClickOut";
 import useEagerConnect from "../hooks/web3/useEagerConnect";
+import Hamburger from "../public/hamburger.svg";
 import { DropdownActions } from "./Dropdowns/DropdownActions";
 import { useConnectWalletModalOpen } from "./Modals/ConnectWalletModal";
 import { ShortenedWallet } from "./ShortenedWallet";
@@ -22,7 +23,6 @@ const Account = (props) => {
   const ref = useRef();
   useOnClickOut(ref, () => setDropdownOpen(false));
 
-  const router = useRouter();
   // need to do something more intelligent here
   // if (error) {
   //   return (
@@ -51,42 +51,77 @@ const Account = (props) => {
     >
       <ShortenedWallet wallet={account} />
       <div className="w-2" />
-      <Image
-        className={dropdownOpen ? "-rotate-90" : "rotate-90"}
-        src={"/chevron.svg"}
-        onClick={() => setDropdownOpen(!dropdownOpen)}
-        alt="dropdown"
-        height={16}
-        width={16}
+      <AccountDropdown
+        dropdownOpen={dropdownOpen}
+        setDropdownOpen={setDropdownOpen}
       />
-      {dropdownOpen && (
-        <DropdownActions bottom={-140}>
-          <div
-            className="cursor-pointer m-4 text-center text-xl hover:opacity-50 flex items-center"
-            onClick={() => {
-              deactivate();
-              setDropdownOpen(false);
-            }}
-          >
-            Disconnect
-            <div className="w-4" />
-            <Image src="/exit.svg" alt={"disconnect"} height={24} width={24} />
-          </div>
-          <div className="w-4" />
-          <div
-            onClick={() => {
-              router.push("/editprofile");
-              setDropdownOpen(false);
-            }}
-          >
-            <div className="cursor-pointer m-4 text-center text-xl hover:opacity-50 flex items-center">
-              Edit Profile
-            </div>
-          </div>
-          <div className="w-4" />
-        </DropdownActions>
-      )}
     </div>
+  );
+};
+
+const AccountDropdown = (props) => {
+  const { deactivate } = useWeb3React();
+
+  const router = useRouter();
+
+  const { dropdownOpen, setDropdownOpen } = props;
+  return (
+    <>
+      <div
+        className="hidden sm:block"
+        onClick={() => setDropdownOpen(!dropdownOpen)}
+      >
+        <Image
+          className={dropdownOpen ? "-rotate-90" : "rotate-90"}
+          src={"/chevron.svg"}
+          alt="dropdown"
+          height={16}
+          width={16}
+        />
+      </div>
+
+      <div
+        className="block sm:hidden"
+        onClick={() => setDropdownOpen(!dropdownOpen)}
+      >
+        <Hamburger fill="white" height={24} width={24} />
+      </div>
+
+      {dropdownOpen && (
+        <div className="absolute sm:top-14 bottom-16">
+          <DropdownActions>
+            <div
+              className="cursor-pointer m-4 text-center text-xl hover:opacity-50 flex items-center"
+              onClick={() => {
+                deactivate();
+                setDropdownOpen(false);
+              }}
+            >
+              Disconnect
+              <div className="w-4" />
+              <Image
+                src="/exit.svg"
+                alt={"disconnect"}
+                height={24}
+                width={24}
+              />
+            </div>
+            <div className="w-4" />
+            <div
+              onClick={() => {
+                router.push("/editprofile");
+                setDropdownOpen(false);
+              }}
+            >
+              <div className="cursor-pointer m-4 text-center text-xl hover:opacity-50 flex items-center">
+                Edit Profile
+              </div>
+            </div>
+            <div className="w-4" />
+          </DropdownActions>
+        </div>
+      )}
+    </>
   );
 };
 
