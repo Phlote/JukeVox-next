7d5d1292c8e08e132a2b999fe6be2ddfdd18a8d8
Michael Anderjaska <m.anderjaska@gmail.com>
2022-04-19 16:56:19 +0100

    tell them to connect w polygon

--- utils/connectors.ts
+++ utils/connectors.ts
@@ -3,13 +3,22 @@ import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { SupportedChainId } from "./constants";
 
 export const Injected = new InjectedConnector({
-  supportedChainIds: [SupportedChainId.RINKEBY, SupportedChainId.POLYGON],
+  supportedChainIds: [
+    SupportedChainId.MAINNET,
+    SupportedChainId.RINKEBY,
+    SupportedChainId.POLYGON,
+  ],
 });
 
 export const WalletConnect = new WalletConnectConnector({
-  supportedChainIds: [SupportedChainId.RINKEBY, SupportedChainId.POLYGON],
+  supportedChainIds: [
+    // SupportedChainId.MAINNET,
+    SupportedChainId.RINKEBY,
+    SupportedChainId.POLYGON,
+  ],
   bridge: "https://bridge.walletconnect.org",
   rpc: {
+    // [SupportedChainId.MAINNET]: `https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_MAINNET}`,
     [SupportedChainId.POLYGON]: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_POLYGON}`,
     [SupportedChainId.RINKEBY]: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_RINKEBY}`,
   },
