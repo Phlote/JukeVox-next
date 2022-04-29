14ad42c62bfc26641d9bd8c4af17b9b84bab8bd7
Michael Anderjaska <m.anderjaska@gmail.com>
2022-04-29 18:25:09 +0300

    forgot a connect wallet button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
+import { useWeb3React } from "@web3-react/core";
 import React from "react";
 import { Footer } from "../components/Footer";
 import { HollowInputContainer } from "../components/Hollow";
 import Layout from "../components/Layouts";
+import { useConnectWalletModalOpen } from "../components/Modals/ConnectWalletModal";
 import { useSubmissionFlowOpen } from "../components/SubmissionFlow";
 
 function Home() {
   const [, setOpen] = useSubmissionFlowOpen();
+
+  const { account } = useWeb3React();
+  const [, setConnectWalletOpen] = useConnectWalletModalOpen();
 
   return (
     <div className="flex flex-col justify-center items-center relative w-full">
@@ -28,9 +33,12 @@ function Home() {
           <div className="w-80 h-16 cursor-pointer hover:opacity-75 shadow-sm">
             <HollowInputContainer
               style={{ justifyContent: "center", border: "1px solid white" }}
-              onClick={() => setOpen(true)}
+              onClick={() => {
+                if (account) setOpen(true);
+                else setConnectWalletOpen(true);
+              }}
             >
-              Submit music to Phlote
+              {account ? "Submit music to Phlote" : "Connect"}
             </HollowInputContainer>
           </div>
         </div>
