c9327b888728e55000a90ca0a95cf79406c00f3d
Michael Anderjaska <m.anderjaska@gmail.com>
2022-05-04 19:07:27 -0400

    update

--- pages/submission/[id].tsx
+++ pages/submission/[id].tsx
@@ -54,7 +54,7 @@ export default function SubmissionPage(props) {
         <div className="h-8" />
         <TwitterShareButton
           url={`${process.env.NEXT_PUBLIC_VERCEL_URL}/submission/${submission.id}`}
-          title={`Have you heard ${submission.mediaTitle}? It's a 💎\n`}
+          title={`Have you heard ${submission.mediaTitle}? It's a 💎.`}
         >
           <TwitterIcon size={32} round />
         </TwitterShareButton>
