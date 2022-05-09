769effa629f182c581f2ba2cfcd0d5e0f1850622
Zachary Fogg <me@zfo.gg>
2022-05-09 16:07:49 -0400

    i think this fixes the deploy idk

--- solidity/contracts/lib/IPFSStorage.sol
+++ solidity/contracts/lib/IPFSStorage.sol
@@ -16,24 +16,24 @@ pragma solidity ^0.8.9;
  * The digest is 32 bytes long and can be stored using bytes32 efficiently.
  */
 contract IPFSStorage {
-  struct Multihash {
-    bytes32 digest;
-    uint8 hashFunction;
-    uint8 size;
-  }
+    struct Multihash {
+        bytes32 digest;
+        uint8 hashFunction;
+        uint8 size;
+    }
 
-  mapping (address => Multihash) private entries;
+    mapping (address => Multihash) private entries;
 
-  event EntrySet(
-    address indexed key,
-    bytes32 digest,
-    uint8 hashFunction,
-    uint8 size
-  );
+    event EntrySet(
+        address indexed key,
+        bytes32 digest,
+        uint8 hashFunction,
+        uint8 size
+    );
 
-  event EntryDeleted(
-    address indexed key
-  );
+    event EntryDeleted(
+        address indexed key
+    );
 
   /**
    * @dev associate a multihash entry with the sender address
@@ -41,39 +41,39 @@ contract IPFSStorage {
    * @param _hashFunction hashFunction code for the hash function used
    * @param _size length of the digest
    */
-  function setEntry(bytes32 _digest, uint8 _hashFunction, uint8 _size)
-  public
-  {
-    Multihash memory entry = Multihash(_digest, _hashFunction, _size);
-    entries[msg.sender] = entry;
-    emit EntrySet(
-      msg.sender, 
-      _digest, 
-      _hashFunction, 
-      _size
-    );
-  }
+    function setEntry(bytes32 _digest, uint8 _hashFunction, uint8 _size)
+    public
+    {
+        Multihash memory entry = Multihash(_digest, _hashFunction, _size);
+        entries[msg.sender] = entry;
+        emit EntrySet(
+            msg.sender,
+            _digest,
+            _hashFunction,
+            _size
+        );
+    }
 
-  /**
-   * @dev deassociate any multihash entry with the sender address
-   */
-  function clearEntry()
-  public
-  {
-    require(entries[msg.sender].digest != 0, "already empty");
-    delete entries[msg.sender];
-    emit EntryDeleted(msg.sender);
-  }
+    /**
+    * @dev deassociate any multihash entry with the sender address
+    */
+    function clearEntry()
+    public
+    {
+        require(entries[msg.sender].digest != 0, "already empty");
+        delete entries[msg.sender];
+        emit EntryDeleted(msg.sender);
+    }
 
-  /**
-   * @dev retrieve multihash entry associated with an address
-   * @param _address address used as key
-   */
-  function getEntry(address _address)
-  public view
-  returns(bytes32 digest, uint8 hashfunction, uint8 size)
-  {
-    Multihash storage entry = entries[_address];
-    return (entry.digest, entry.hashFunction, entry.size);
-  }
+    /**
+    * @dev retrieve multihash entry associated with an address
+    * @param _address address used as key
+    */
+    function getEntry(address _address)
+    public view
+    returns(bytes32 digest, uint8 hashfunction, uint8 size)
+    {
+        Multihash storage entry = entries[_address];
+        return (entry.digest, entry.hashFunction, entry.size);
+    }
 }
