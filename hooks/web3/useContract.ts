8b21ad8c98627e3bd7b87210582078c9f576b34f
Michael Anderjaska <m.anderjaska@gmail.com>
2022-05-24 13:35:59 -0400

    annoyance

--- hooks/web3/useContract.ts
+++ hooks/web3/useContract.ts
@@ -2,7 +2,7 @@ import { Contract } from "@ethersproject/contracts";
 import { useWeb3React } from "@web3-react/core";
 import { useMemo } from "react";
 
-export default function useContract<T extends Contract = Contract>(
+export default function useContract<T extends Contract>(
   address: string,
   ABI: any
 ): T | null {
