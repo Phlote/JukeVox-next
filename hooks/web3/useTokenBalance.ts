6dfc477ab727899407c5f987a95fcc374356231e
Michael Anderjaska <m.anderjaska@gmail.com>
2022-05-24 14:40:31 -0400

    could be useful later

--- hooks/web3/useTokenBalance.ts
+++ hooks/web3/useTokenBalance.ts
@@ -1,37 +1,32 @@
-import useSWR from "swr";
-import type { ERC20 } from "../../solidity/typechain";
-import useKeepSWRDataLiveAsBlocksArrive from "./useKeepSWRDataLiveAsBlocksArrive";
-import useTokenContract from "./useTokenContract";
+// function getTokenBalance(contract: ERC20) {
+//   return async (_: string, address: string) => {
+//     const balance = await contract.balanceOf(address);
 
-function getTokenBalance(contract: ERC20) {
-  return async (_: string, address: string) => {
-    const balance = await contract.balanceOf(address);
+//     return balance;
+//   };
+// }
 
-    return balance;
-  };
-}
+// export default function useTokenBalance(
+//   address: string,
+//   tokenAddress: string,
+//   suspense = false
+// ) {
+//   const contract = useTokenContract(tokenAddress);
 
-export default function useTokenBalance(
-  address: string,
-  tokenAddress: string,
-  suspense = false
-) {
-  const contract = useTokenContract(tokenAddress);
+//   const shouldFetch =
+//     typeof address === "string" &&
+//     typeof tokenAddress === "string" &&
+//     !!contract;
 
-  const shouldFetch =
-    typeof address === "string" &&
-    typeof tokenAddress === "string" &&
-    !!contract;
+//   const result = useSWR(
+//     shouldFetch ? ["TokenBalance", address, tokenAddress] : null,
+//     getTokenBalance(contract),
+//     {
+//       suspense,
+//     }
+//   );
 
-  const result = useSWR(
-    shouldFetch ? ["TokenBalance", address, tokenAddress] : null,
-    getTokenBalance(contract),
-    {
-      suspense,
-    }
-  );
+//   useKeepSWRDataLiveAsBlocksArrive(result.mutate);
 
-  useKeepSWRDataLiveAsBlocksArrive(result.mutate);
-
-  return result;
-}
+//   return result;
+// }
