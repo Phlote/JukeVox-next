d12168ab2f169e199fb5621cccebfb888b14864c
Michael Anderjaska <m.anderjaska@gmail.com>
2022-07-19 21:45:10 +0100

    scratch that

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -39,7 +39,6 @@ export const uploadFiles = async (args: uploadFilesArguments) => {
 
     if (publicURLQuery.error) throw publicURLQuery.error;
 
-    //You could also return the moralis uri here as well
     return publicURLQuery.publicURL;
   } catch (e) {
     console.error(e);
