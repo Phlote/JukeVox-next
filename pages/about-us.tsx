7b123699c4c309249d2c641909f518ffe5f56e75
Théo Carrara <theocarraraleao@gmail.com>
2022-10-27 19:49:32 +0200

    Temp work for about us and how it works
    Make the screens more minimal

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -6,6 +6,13 @@ import { useConnectWalletModalOpen } from "../components/Modals/ConnectWalletMod
 import { useSubmissionFlowOpen } from "../components/SubmissionFlow";
 import AboutUsContent from "../components/AboutUsContent";
 
+let TempAboutUs = ()=>
+  <section className="flex items-center justify-center mt-14 sm:py-20 sm:mt-20 lg:mt-38">
+    <div className="w-9/12 sm:w-10/12 2xl:w-full flex flex-col justify-center items-center pt-10 gap-4">
+      <h1 className="text-2xl sm:text-7xl font-extrabold">Phlote delivers the best new genre-bending music from around the world to our members as exclusive music NFTs.</h1>
+    </div>
+  </section>
+
 function AboutUs() { // LANDING PAGE
   const [, setOpen] = useSubmissionFlowOpen();
 
@@ -14,7 +21,8 @@ function AboutUs() { // LANDING PAGE
 
   return (
     <div className="font-roboto">
-      <AboutUsContent />
+      {/*<AboutUsContent />*/}
+      <TempAboutUs/>
     </div>
   );
 }
