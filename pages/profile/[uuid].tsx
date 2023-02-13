c71591407018b1f10375b5b6862f0610649ab25b
Homam Malkawi <homammalk2@gmail.com>
2023-02-13 00:53:24 -0800

    text improvment

--- pages/profile/[uuid].tsx
+++ pages/profile/[uuid].tsx
@@ -46,7 +46,7 @@ export default function Profile(props) {
             <>
               <div className="flex-grow"></div>
               <label className="relative flex justify-between items-center group p-2 text-xl">
-                {userCosigns? "Your Cosigns" : "Your Submissions"}
+                {userCosigns? `Show Cosigns: ${cosignedSubmissions.data.length}` : `Show Submissions: ${submissions.length}`}
                 <input type="checkbox" className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md" onClick={() => setUserCosigns(!userCosigns)} />
                 <span className="w-16 h-10 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
               </label>
