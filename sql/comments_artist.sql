29654c39e6652a764c6640dbe12bfdd2026027bf
Théo Carrara <theocarraraleao@gmail.com>
2023-02-22 23:08:54 +0100

    edited sql files to fit current db
    also migrated the db locally for better error logging

--- sql/comments_artist.sql
+++ sql/comments_artist.sql
@@ -1,18 +1,18 @@
-drop table if exists votes_curator cascade;
-drop table if exists comments_curator cascade;
+drop table if exists votes_artist cascade;
+drop table if exists comments_artist cascade;
 
--- Create a table for comments_curator
-create table comments_curator (
+-- Create a table for comments_artist
+create table comments_artist (
     id bigserial not null,
     slug text not null unique,
-    threadId uuid not null references "Curator_Submission_Table" ("submissionID"),
+    threadId uuid not null references "Artist_Submission_Table" ("submissionID"),
     "createdAt" timestamp with time zone default now() not null,
     "updatedAt" timestamp with time zone default now() null,
     title text null,
     content text null,
     "isPublished" boolean default false not null,
     "authorId" text not null,
-    "parentId" bigint null references comments_curator (id),
+    "parentId" bigint null references comments_artist (id),
     live boolean default true null,
     "isPinned" boolean default false not null,
     "isDeleted" boolean default false not null,
@@ -22,9 +22,9 @@ create table comments_curator (
     unique (slug)
 );
 
--- Create a table for votes_curator
-create table votes_curator (
-    "commentId" bigint not null references comments_curator (id),
+-- Create a table for votes_artist
+create table votes_artist (
+    "commentId" bigint not null references comments_artist (id),
     "userId" text not null,
     "value" int not null,
 
@@ -37,16 +37,16 @@ begin;
   drop publication if exists supabase_realtime;
   create publication supabase_realtime;
 commit;
-alter publication supabase_realtime add table comments_curator, votes_curator, "Users_Table";
+alter publication supabase_realtime add table comments_artist, votes_artist, "Users_Table";
 
 
-drop view if exists comments_curator_thread_with_user_vote;
-drop view if exists comments_curator_thread;
-drop view if exists comments_curator_with_author_votes_curator;
-drop view if exists comments_curator_linear_view;
-drop view if exists comment_with_author;
+drop view if exists comments_artist_thread_with_user_vote;
+drop view if exists comments_artist_thread;
+drop view if exists comments_artist_with_author_votes_artist;
+drop view if exists comments_artist_linear_view;
+drop view if exists artist_comment_with_author;
 
-create view comment_with_author as
+create view artist_comment_with_author as
     select
         c.id,
         c.slug,
@@ -63,18 +63,18 @@ create view comment_with_author as
         c."isApproved",
         to_jsonb(u) as author
     from
-        comments_curator c
+        comments_artist c
         left join "Users_Table" u on c."authorId" = u.wallet;
 
-create view comments_curator_linear_view as
+create view comments_artist_linear_view as
     select
         root_c.*,
         to_jsonb(parent_c) as parent,
         coalesce(json_agg(children_c) filter (where children_c.id is not null), '[]') as responses
     from
-        comment_with_author root_c
-        inner join comment_with_author parent_c on root_c."parentId" = parent_c.id
-        left join comment_with_author children_c on children_c."parentId" = root_c.id
+        artist_comment_with_author root_c
+        inner join artist_comment_with_author parent_c on root_c."parentId" = parent_c.id
+        left join artist_comment_with_author children_c on children_c."parentId" = root_c.id
     group by
         root_c.id,
         root_c.slug,
@@ -92,7 +92,7 @@ create view comments_curator_linear_view as
         root_c.author,
         parent_c.*;
 
-create or replace view comments_curator_with_author_votes_curator as
+create or replace view comments_artist_with_author_votes_artist as
     select
         c.id,
         c.slug,
@@ -111,18 +111,18 @@ create or replace view comments_curator_with_author_votes_curator as
         coalesce (
             sum (v.value) over w,
             0
-        ) as "votes_curator",
-        sum (case when v.value > 0 then 1 else 0 end) over w as "upvotes_curator",
-        sum (case when v.value < 0 then 1 else 0 end) over w as "downvotes_curator"
+        ) as "votes_artist",
+        sum (case when v.value > 0 then 1 else 0 end) over w as "upvotes_artist",
+        sum (case when v.value < 0 then 1 else 0 end) over w as "downvotes_artist"
         -- (select case when auth.uid() = v."userId" then v.value else 0 end) as "userVoteValue"
     from
-        comment_with_author c
-        left join votes_curator v on c.id = v."commentId"
+        artist_comment_with_author c
+        left join votes_artist v on c.id = v."commentId"
     window w as (
         partition by v."commentId"
     );
 
-create recursive view comments_curator_thread (
+create recursive view comments_artist_thread (
     id,
     slug,
     "createdAt",
@@ -137,12 +137,12 @@ create recursive view comments_curator_thread (
     "isDeleted",
     "isApproved",
     "author",
-    "votes_curator",
-    "upvotes_curator",
-    "downvotes_curator",
+    "votes_artist",
+    "upvotes_artist",
+    "downvotes_artist",
     "depth",
     "path",
-    "pathvotes_curatorRecent",
+    "pathvotes_artistRecent",
     "pathLeastRecent",
     "pathMostRecent"
 ) as
@@ -161,16 +161,16 @@ create recursive view comments_curator_thread (
         "isDeleted",
         "isApproved",
         "author",
-        "votes_curator",
-        "upvotes_curator",
-        "downvotes_curator",
+        "votes_artist",
+        "upvotes_artist",
+        "downvotes_artist",
         0 as depth,
         array[id] as "path",
-        array[id] as "pathvotes_curatorRecent",
+        array[id] as "pathvotes_artistRecent",
         array[id] as "pathLeastRecent",
         array[id] as "pathMostRecent"
     from
-        comments_curator_with_author_votes_curator
+        comments_artist_with_author_votes_artist
     where
         "parentId" is null
     union
@@ -189,19 +189,19 @@ create recursive view comments_curator_thread (
         p1."isDeleted",
         p1."isApproved",
         p1."author",
-        p1."votes_curator",
-        p1."upvotes_curator",
-        p1."downvotes_curator",
+        p1."votes_artist",
+        p1."upvotes_artist",
+        p1."downvotes_artist",
         p2.depth + 1 as depth,
         p2."path" || p1.id::bigint as "path",
-        p2."pathvotes_curatorRecent" || -p1."votes_curator"::bigint || -extract(epoch from p1."createdAt")::bigint || p1.id as "pathvotes_curatorRecent",
+        p2."pathvotes_artistRecent" || -p1."votes_artist"::bigint || -extract(epoch from p1."createdAt")::bigint || p1.id as "pathvotes_artistRecent",
         p2."pathLeastRecent" || extract(epoch from p1."createdAt")::bigint || p1.id as "pathLeastRecent",
         p2."pathMostRecent" || -extract(epoch from p1."createdAt")::bigint || p1.id as "pathMostRecent"
     from
-        comments_curator_with_author_votes_curator p1
-        join comments_curator_thread p2 on p1."parentId" = p2.id;
+        comments_artist_with_author_votes_artist p1
+        join comments_artist_thread p2 on p1."parentId" = p2.id;
 
-create or replace view comments_curator_thread_with_user_vote as
+create or replace view comments_artist_thread_with_user_vote as
     select distinct on (id)
         id,
         slug,
@@ -217,12 +217,12 @@ create or replace view comments_curator_thread_with_user_vote as
         "isDeleted",
         "isApproved",
         "author",
-        "votes_curator",
-        "upvotes_curator",
-        "downvotes_curator",
+        "votes_artist",
+        "upvotes_artist",
+        "downvotes_artist",
         "depth",
         "path",
-        "pathvotes_curatorRecent",
+        "pathvotes_artistRecent",
         "pathLeastRecent",
         "pathMostRecent",
         coalesce(
@@ -230,10 +230,10 @@ create or replace view comments_curator_thread_with_user_vote as
                 select
                     v."value"
                 from
-                    votes_curator v
+                    votes_artist v
                 where
                      v."commentId" = id
             ),
             0
         ) as "userVoteValue"
-    from comments_curator_thread
+    from comments_artist_thread
